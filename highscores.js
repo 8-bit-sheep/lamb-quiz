4d073e351472e6088ab5ed2da62d37cca2e4f886
Otso <oer.kallinen@gmail.com>
2019-07-16 12:13:32 +0300

    fetch highscores from spreadsheet

--- highscores.js
+++ highscores.js
@@ -1,25 +1,35 @@
-const highscoreList = document.getElementById("highscoreList");
-const id = window.location.search.split("=")[1];
-const highscores = JSON.parse(localStorage.getItem("highscores" + id)) || [];
-let contentName = document.getElementById("contentName");
+d3.csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTKBOji3CenMyR-hvdBIBZTYL82uHhMHOoUIbiK1XYr2CycXkVBY96aWeVnfp77mkELAlNIR0Dk934g/pub?output=csv"
+)
+  .then(highscores => {
+    const id = window.location.search.split("=")[1];
+    const ranking = highscores.filter(x => x.gameId === id).sort((a, b) => parseInt(b.score) > parseInt(a.score)).slice(10);
+    const highscoreList = document.getElementById("highscoreList");
+    let contentName = document.getElementById("contentName");
 
-highscoreList.innerHTML = highscores
-  .map(
-    score =>
-      `<tr>
-    <th class="highscoreNames">${score.name.substring(
-      0,
-      16
-    )}</th><th class="highscoreScores">${score.score}</th></tr>`
-  )
-  .join("");
+    highscoreList.innerHTML = ranking
+      .map(
+        row =>
+          `<tr>
+    <th class="highscoreNames">${row.name.substring(
+            0,
+            16
+          )}</th><th class="highscoreScores">${row.score}</th></tr>`
+      )
+      .join("");
 
-if (id === "1") {
-  contentName.innerText = "Crash Course Computer Science";
-}
-if (id === "2") {
-  contentName.innerText = "JavaScript";
-}
-if (id === "0") {
-  contentName.innerText = "All Content";
-}
+    d3.csv(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfBk-rwrIauBPn7iuoLXBxP2sSYOXRYCbJ2GflzSK6wxGVGDr_fAqORJ0JWPdajFLxnGegmrlI26HB/pub?output=csv"
+    )
+      .then(
+        names => {        
+          const contentNames = (id !== "0") ? 
+          names.filter(x => x.contentId === id)[0].content :
+          "All";
+          
+          contentName.innerText = contentNames;
+        }
+      )
+
+
+  })
--- index.js
+++ index.js
@@ -4,11 +4,10 @@ let questions = [];
 d3.csv(
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfBk-rwrIauBPn7iuoLXBxP2sSYOXRYCbJ2GflzSK6wxGVGDr_fAqORJ0JWPdajFLxnGegmrlI26HB/pub?output=csv"
 ).then(data => {
-  questions = data;
-  getData();
+  getData(data);
 });
 
-const getData = () => {
+const getData = questions => {
   const contentTitles = [
     ...new Set(questions.map(question => question.content))
   ];
@@ -27,27 +26,27 @@ const createLinkBtn = (text, id) => {
   const div = document.createElement("div");
   contentBox.appendChild(div);
   const h4 = document.createElement("h4");
-    div.appendChild(h4);
-    h4.innerText = text;
-    const gameLink = document.createElement("a");
-    div.appendChild(gameLink);
-    const buttonText = document.createElement("p")
-    gameLink.appendChild(buttonText)
-    buttonText.classList.add("btn-link");
-    buttonText.innerText = "Play!";
-    const gameHref = document.createAttribute("href");
-    gameLink.setAttributeNode(gameHref);
+  div.appendChild(h4);
+  h4.innerText = text;
+  const gameLink = document.createElement("a");
+  div.appendChild(gameLink);
+  const buttonText = document.createElement("p")
+  gameLink.appendChild(buttonText)
+  buttonText.classList.add("btn-link");
+  buttonText.innerText = "Play!";
+  const gameHref = document.createAttribute("href");
+  gameLink.setAttributeNode(gameHref);
   gameHref.value = "game.html?contentId=" + id;
-    const highScoreLink = document.createElement("a");
-    //highScoreLink.classList.add("btn-gamelink");
-    div.appendChild(highScoreLink);
-    const hsbuttonText = document.createElement("p")
-    highScoreLink.appendChild(hsbuttonText)
-    hsbuttonText.classList.add("btn-link");
+  const highScoreLink = document.createElement("a");
+  //highScoreLink.classList.add("btn-gamelink");
+  div.appendChild(highScoreLink);
+  const hsbuttonText = document.createElement("p")
+  highScoreLink.appendChild(hsbuttonText)
+  hsbuttonText.classList.add("btn-link");
 
-    const highScoreHref = document.createAttribute("href");
-    highScoreHref.value = "highscores.html?contentId=" + id;
+  const highScoreHref = document.createAttribute("href");
+  highScoreHref.value = "highscores.html?contentId=" + id;
   highScoreLink.setAttributeNode(highScoreHref);
   div.classList.add("content-container");
-    hsbuttonText.innerText = "High Scores";
+  hsbuttonText.innerText = "High Scores";
 };
