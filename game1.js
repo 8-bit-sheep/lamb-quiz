46a72d34df202a3e9cd552c161de1949e753da80
selma-lindfors <selma.lindfors@gmail.com>
2019-07-29 12:59:50 +0300

    added another game file with open questions

--- game1.js
+++ game1.js
@@ -1,9 +1,3 @@
-const parseUrlParameters = parameter =>
-  parameter //"?key1=value1&key2=value2&key3=value3"
-    .substr(1) //"key1=value1&key2=value2&key3=value3"
-    .split("&") //["key1=value1" "key2=value2" key3=value3"]
-    .map(el => el.split("=")) //[["key1" "value1"] ["key2" "value2"] [key3" "value3"]]
-    .reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {});
 // dom references
 
 const loader = document.getElementById("loader");
@@ -15,39 +9,38 @@ const scoreText = document.getElementById("score");
 const episodeNameText = document.getElementById("episodeName");
 const contentNameText = document.getElementById("contentName");
 const urlNameText = document.getElementById("episodeurl");
-const params = parseUrlParameters(window.location.search);
-const contentId = params.contentId;
-const contributed = params.contributed;
+const id = window.location.search.split("=")[1];
 const segmentBox = document.getElementById("segment-index");
 const segmentButtons = document.getElementById("segment-buttons");
-
+const answerForm = document.getElementById("answer-form");
+const choiceBox = document.getElementById("choice-box");
+const saveAnswerBtn = document.getElementById("saveAnswerButton");
+const answer = document.getElementById("answer");
 // state
 let currentQuestion = {};
 let acceptingAnswers = false;
-let continueToNext = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let questions = [];
+let cont = false;
 let maxQuestions = 20;
 let isSegmentGame = false;
 
-// config
+// state
 const CORRECT_BONUS = 10;
 
 const startGame = () => {
-  if (!contributed && contentId !== "0")
-    window.location.assign("user-questions.html?contentId=" + contentId);
   questionCounter = 0;
   score = 0;
   scoreText.innerText = score;
   loader.classList.add("hidden");
-  if (contentId === "0") {
+  if (id === "0") {
     game.classList.remove("hidden");
     availableQuestions = [...questions];
     maxQuestions = 50;
     getNewQuestion();
-  } else if (contributed) {
+  } else {
     segmentBox.classList.remove("hidden");
     segmentSelector();
   }
@@ -57,16 +50,10 @@ const segmentSelector = () => {
   const segmentList = [
     ...new Set(questions.map(question => question.contentSegment))
   ];
-  if (segmentList.length < 2) {
-    game.classList.remove("hidden");
-    availableQuestions = [...questions];
-    segmentBox.classList.add("hidden");
-    getNewQuestion();
-  }
-  createButtonInsideListItem(document.getElementById("all-btns"), "Play All!");
   segmentList.forEach(segment =>
     createButtonInsideListItem(segmentButtons, segment)
   );
+  createButtonInsideListItem(segmentButtons, "Play All!");
 };
 
 const createButtonInsideListItem = (list, text) => {
@@ -75,11 +62,7 @@ const createButtonInsideListItem = (list, text) => {
   const button = document.createElement("button");
   li.appendChild(button);
   button.innerText = text;
-  if (text === "Play All!") {
-    button.classList.add("all-btn");
-  } else {
-    button.classList.add("segment-btn");
-  }
+  button.classList.add("segment-btn");
   button.addEventListener("click", startSegmentGame);
 };
 
@@ -93,7 +76,7 @@ const startSegmentGame = e => {
     isSegmentGame = true;
   }
   if (availableQuestions.length < maxQuestions) {
-    maxQuestions = availableQuestions.length;
+    availableQuestions.length = maxQuestions;
   }
   getNewQuestion();
   game.classList.remove("hidden");
@@ -101,15 +84,13 @@ const startSegmentGame = e => {
 };
 
 const getNewQuestion = () => {
-  continueToNext = false;
+  cont = false;
   if (availableQuestions.length === 0 || questionCounter > maxQuestions - 1) {
     localStorage.setItem("mostRecentScore", score);
     if (isSegmentGame === false) {
-      return window.location.assign(
-        "end.html?contentId=" + contentId + "&all=1"
-      );
+      return window.location.assign("end.html?contentId=" + id + "&all=1");
     } else {
-      return window.location.assign("end.html?contentId=" + contentId);
+      return window.location.assign("end.html?contentId=" + id);
     }
   }
 
@@ -121,11 +102,27 @@ const getNewQuestion = () => {
   contentNameText.innerText = currentQuestion.content;
   question.innerText = currentQuestion.question;
   urlNameText.href = currentQuestion.url;
-  choices.forEach(choice => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
-  });
 
+  if (currentQuestion.open === "TRUE") {
+    answerForm.classList.remove("hidden");
+    choiceBox.classList.add("hidden");
+    answer.addEventListener(
+      "keyup",
+      () => (saveAnswerBtn.disabled = !answer.value)
+    );
+    document.getElementById("saveAnswerButton").addEventListener("click", e => {
+      e.preventDefault();
+      console.log(
+        answer.value === currentQuestion["choice" + currentQuestion.answer]
+      );
+      getNewQuestion();
+    });
+  } else {
+    choices.forEach(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
+    });
+  }
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
@@ -137,12 +134,12 @@ const wrongAnswer = () => {
         choice.parentElement.classList.add("correct");
       }
     });
-    continueToNext = true;
+    cont = true;
   }, 200);
 };
 
 const continueGame = () => {
-  if (continueToNext) {
+  if (cont) {
     choices.forEach(choice => {
       choice.parentElement.classList.remove(["incorrect"]);
       choice.parentElement.classList.remove(["correct"]);
@@ -151,57 +148,31 @@ const continueGame = () => {
   } else return;
 };
 
-checkChoice = (selectedAnswer, selectedChoice) => {
-  const classToApply =
-    selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
-
-  if (classToApply === "correct") {
-    incrementScore(CORRECT_BONUS);
-  }
-  selectedChoice.parentElement.classList.add(classToApply);
-
-  if (classToApply === "correct") {
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 500);
-  } else {
-    acceptingAnswers = false;
-    wrongAnswer();
-  }
-};
-
-const keyboardMap = {
-  49: "1",
-  50: "2",
-  51: "3",
-  52: "4"
-};
-
-const keypress = kp => {
-  if (!acceptingAnswers | continueToNext) {
-    continueGame();
-  } else {
-    choices.forEach(choice => {
-      if (choice.dataset["number"] === keyboardMap[kp]) {
-        selectedChoice = choice;
-      }
-    });
-    checkChoice(keyboardMap[kp], selectedChoice);
-  }
-};
-
-document.onkeyup = e => keypress(e.which);
-
 document.body.addEventListener("click", e => continueGame());
 
 choices.forEach(choice => {
   choice.addEventListener("click", e => {
-    if (!acceptingAnswers | continueToNext) return;
+    if (!acceptingAnswers | cont) return;
     acceptinganswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
-    checkChoice(selectedAnswer, selectedChoice);
+    const classToApply =
+      selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    if (classToApply === "correct") {
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 500);
+    } else {
+      acceptingAnswers = false;
+      wrongAnswer();
+    }
   });
 });
 
@@ -214,8 +185,8 @@ d3.csv(
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfBk-rwrIauBPn7iuoLXBxP2sSYOXRYCbJ2GflzSK6wxGVGDr_fAqORJ0JWPdajFLxnGegmrlI26HB/pub?output=csv"
 ).then(data => {
   questions = data.filter(question => {
-    if (contentId === "0") return true;
-    else return question.contentId === contentId;
+    if (id === "0") return true;
+    else return question.contentId === id;
   });
   startGame();
 });
